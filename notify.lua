5020d78ed6bd6f98fccf19504fc2be958b6a9c0e
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-11-18 21:22:06 +0100

    Update notify.lua

--- notify.lua
+++ notify.lua
@@ -1,4 +1,10 @@
 function Notify(title, text, style)
-    -- Insert your own notification export here. Title is the subject, Text is the string that is passed on trough the client. Style is the okokNotify way of different icons.
-    exports['okokNotify']:Alert(title, text, 5000, style)
+    local success, err = pcall(function()
+        -- Insert your own notification export here. Title is the subject, Text is the string that is passed on trough the client. Style is the okokNotify way of different icons.
+        exports['okokNotify']:Alert(title, text, 5000, style)
+    end)
+
+    if not success then
+        print(title, text)
+    end
 end
